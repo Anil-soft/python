2d0668ec7709a897de0b52d4ba017310b946662e
Anil7101 <mala.anilkumar@hitachivantara.com>
2025-02-20 22:22:44 +0530

    string types

---
+++ string-datatype/python.py
@@ -0,0 +1,33 @@
+# example:-1
+
+str1 = "anil"
+str2 = "how are you"
+result = str1 + " " + str2
+print (result)
+
+# example:-2
+
+name = "anil"
+age = 28
+result = name + " is " + str(age) + " years old! "
+print (result)
+
+#🔹 Note: You must convert numbers to strings using str(age) before concatenation.
+
+# example:-3
+
+name = "Ashwini"
+age = 25
+result = f"{name} age is {age} old!"
+print (result)
+
+# example:-4
+
+num1 = 32
+num2 = 33
+sum = f"sum of {num1} and {num2} is {num1 + num2}"
+print(sum)
+
+# 🚀 No need to use + or str() for type conversion!
+# The variable age (integer) is automatically converted to a string.
+
