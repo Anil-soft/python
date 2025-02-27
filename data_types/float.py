2761c6d80149b5e096bf6f86cac1506bd02f917d
Anil7101 <mala.anilkumar@hitachivantara.com>
2025-02-27 08:50:53 +0530

    float data type

---
+++ data_types/float.py
@@ -0,0 +1,48 @@
+# 📌 1️⃣ Store and Print a Float Value
+# ✅ Example:-1
+
+price = 99.99
+print ("price:", price)
+
+# 📌 2️⃣ Take a Float Input from User
+# ✅ Example:-2
+
+num = float(input("Enter input value: "))
+print ("You entered:", num)
+
+# 📌 3️⃣ Add Two Float Numbers
+# ✅ Example:-3
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum = num1 + num2
+print("Sum:", sum)
+
+# 📌 4️⃣ Multiply Two Float Numbers
+# ✅ Example:-4
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+product = num1 * num2
+print("Product:", product)
+
+# 📌 5️⃣ Convert Integer to Float
+# ✅ Example:-5
+
+num = 10
+float_num = float(num)
+
+print ("Integer:", num)
+print ("converted to float:", float_num)
+
+# 📌 6️⃣ Convert Float to Integer
+# ✅ Example:-6
+
+num = 10.75
+int_num = int(num)
+
+print ("float value:", num)
+print ("converted int value:", int_num)
+
